6dc0f4dc0db2a940a328d677bd2c2831e829b262
Daniel Turczyniak <danielturczyy@gmail.com>
2026-06-09 11:02:37 +0100

    Added lat and long labels successfully with spacing

--- iss_tracking.py
+++ iss_tracking.py
@@ -12,15 +12,15 @@ root.geometry("1280x610")
 root.resizable(False, False)
 
 # MAP IMAGE
-img = Image.open("world_map.png") # Opens image file
-photo = ImageTk.PhotoImage(img) # Makes it compatible with Tkinter
+img = Image.open("world_map.png")  # Opens image file
+photo = ImageTk.PhotoImage(img)  # Makes it compatible with Tkinter
 
 image_label = tk.Label(root, image=photo)
 image_label.pack()
 
-# WIDGETS FRAME
+# WIDGETS FRAME AND LABELS SETUP
 widgets_frame = tk.Frame(root)
-widgets_frame.pack()
+widgets_frame.place(x=8, y=566)
 
 datetime_label = tk.Label(
     widgets_frame,
@@ -30,9 +30,8 @@ datetime_label = tk.Label(
     padx=10,
     pady=5
 )
-datetime_label.pack()
 
-coords_label = tk.Label(
+lat_label = tk.Label(
     widgets_frame,
     font=("Arial", 14),
     bg="white",
@@ -40,21 +39,37 @@ coords_label = tk.Label(
     padx=10,
     pady=5
 )
-coords_label.pack()
+
+lon_label = tk.Label(
+    widgets_frame,
+    font=("Arial", 14),
+    bg="white",
+    fg="black",
+    padx=10,
+    pady=5
+)
+
+datetime_label.grid(row=0, column=0, padx=5) # All labels placed in a row, equally spaced
+lat_label.grid(row=0, column=1, padx=5)
+lon_label.grid(row=0, column=2, padx=5)
 
 def update():
     try:
-        data = requests.get(url).json()
+        data = requests.get(url).json() # Request from API
         lat = data["iss_position"]["latitude"]
         lon = data["iss_position"]["longitude"]
         time = data["timestamp"]
-        utc_time = datetime.datetime.fromtimestamp(time)
-        datetime_label.config(text=utc_time)
+        utc_time = datetime.datetime.fromtimestamp(time) # Convert from UNIX to UTC time
 
+        datetime_label.config(text=utc_time) # Apply new time to label
+        lat_label.config(text=f"Lat: {lat}")
+        lon_label.config(text=f"Lon: {lon}")
         print(lat, lon, time, utc_time)
 
     except:
-        pass
+        datetime_label.config(text="None")
+        lat_label.config(text=f"Lat: None")
+        lon_label.config(text=f"Lon: None")
 
     root.after(2000, update)
 
