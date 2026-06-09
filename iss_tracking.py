06a4febfc21c6613d97dbb2ad3bea004a91904c7
Daniel Turczyniak <danielturczyy@gmail.com>
2026-06-09 17:42:57 +0100

    coordinates to pixels

--- iss_tracking.py
+++ iss_tracking.py
@@ -5,31 +5,26 @@ import datetime
 
 url = "http://api.open-notify.org/iss-now.json"
 
+
 # GUI SETUP
 root = tk.Tk()
 root.title("ISS Tracking")
 root.geometry("1280x610")
 root.resizable(False, False)
 
-# MAP IMAGE
-img = Image.open("world_map.png")  # Opens image file
-photo = ImageTk.PhotoImage(img)  # Makes it compatible with Tkinter
-
-image_label = tk.Label(root, image=photo)
-image_label.pack()
 
 # WIDGETS FRAME AND LABELS SETUP
 widgets_frame = tk.Frame(root)
 widgets_frame.place(x=8, y=566)
 
-datetime_label = tk.Label(
+utctime_label = tk.Label(
     widgets_frame,
     font=("Arial", 14),
     bg="white",
     fg="black",
     padx=10,
     pady=5
-)
+    )
 
 lat_label = tk.Label(
     widgets_frame,
@@ -38,7 +33,7 @@ lat_label = tk.Label(
     fg="black",
     padx=10,
     pady=5
-)
+    )
 
 lon_label = tk.Label(
     widgets_frame,
@@ -47,27 +42,48 @@ lon_label = tk.Label(
     fg="black",
     padx=10,
     pady=5
-)
+    )
 
-datetime_label.grid(row=0, column=0, padx=5) # All labels placed in a row, equally spaced
+utctime_label.grid(row=0, column=0, padx=5) # All labels placed in a row, equally spaced
 lat_label.grid(row=0, column=1, padx=5)
 lon_label.grid(row=0, column=2, padx=5)
+
+
+# MAP AND DOT SETUP
+img = Image.open("world_map.png")  # Opens image file
+photo = ImageTk.PhotoImage(img)  # Makes it compatible with Tkinter
+
+canvas = tk.Canvas(root, width=1280, height=555) # Canvas that dot can move in
+canvas.create_image(0, 0, image=photo, anchor="nw")
+canvas.pack()
+
+dot = canvas.create_oval(0, 0, 6, 6, fill="red")
+
+def find_position(pLat, pLon):
+    x = ((pLon + 180) / 360) * 1280
+    y = ((90 - pLat) / 180) * 555
+    return int(x), int(y)
 
 def update():
     try:
         data = requests.get(url).json() # Request from API
-        lat = data["iss_position"]["latitude"]
-        lon = data["iss_position"]["longitude"]
+        lat = float(data["iss_position"]["latitude"])
+        lon = float(data["iss_position"]["longitude"])
         time = data["timestamp"]
         utc_time = datetime.datetime.fromtimestamp(time) # Convert from UNIX to UTC time
 
-        datetime_label.config(text=utc_time) # Apply new time to label
+        utctime_label.config(text=utc_time) # Apply new time to label
         lat_label.config(text=f"Lat: {lat}")
         lon_label.config(text=f"Lon: {lon}")
-        print(lat, lon, time, utc_time)
 
+        dot_coords = find_position(lat, lon)
+        dot_x = dot_coords[0]
+        dot_y = dot_coords[1]
+        print(dot_x, dot_y)
+        #canvas.place(dot, x=dot_x, y=dot_y)
+        
     except:
-        datetime_label.config(text="None")
+        utctime_label.config(text="None")
         lat_label.config(text=f"Lat: None")
         lon_label.config(text=f"Lon: None")
 
