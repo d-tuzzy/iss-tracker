786fce4ba388326fb98850a3067c0817bb758222
Daniel Turczyniak <danielturczyy@gmail.com>
2026-06-10 10:17:54 +0100

    Doesn't plot repeated points Comments added

--- iss_tracking.py
+++ iss_tracking.py
@@ -58,12 +58,17 @@ canvas.create_image(0, 0, image=photo, anchor="nw")
 canvas.pack()
 
 
-def find_position(pLat, pLon):
+def find_position(pLat, pLon): # Turn coordinates into discrete pixel values
     x = ((pLon + 180) / 360) * 1280
     y = ((90 - pLat) / 180) * 555
     return int(x), int(y)
 
+
+previous_position = [None, None] # Previous pixel coordinates of dot (where it is on the screen now)
+
 def update():
+    global previous_position
+
     try:
         data = requests.get(url).json() # Request from API
         lat = float(data["iss_position"]["latitude"])
@@ -78,15 +83,20 @@ def update():
         dot_coords = find_position(lat, lon)
         dot_x = dot_coords[0]
         dot_y = dot_coords[1]
-        print(dot_x, dot_y)
-        dot = canvas.create_oval(dot_x, dot_y, dot_x+6, dot_y+6, fill="red")
-        
-    except:
+
+        if (dot_x != previous_position[0]) or (dot_y != previous_position[1]): # Only draw if new pixel coordinates are different
+            dot = canvas.create_oval(dot_x, dot_y, dot_x+6, dot_y+6, fill="red")
+
+        previous_position[0] = dot_x # Can be compared with next iteration's coordinates
+        previous_position[1] = dot_y
+   
+    except Exception as e:
         utctime_label.config(text="None")
         lat_label.config(text=f"Lat: None")
         lon_label.config(text=f"Lon: None")
+        print(e)
 
-    root.after(2000, update)
+    root.after(2000, update) # 2 second delay
 
 update()
 
